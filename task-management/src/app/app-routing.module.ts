37d46609af3ebd8ca6439460817a91d50716ba06
Jgiraldo <jhonnagira@live.com.mx>
2024-09-30 16:43:37 -0500

    install ngrx

--- task-management/src/app/app-routing.module.ts
+++ task-management/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PersonComponent } from './components/person/person.component';
-import { TaskComponent } from './components/task/task.component';
 
 const routes: Routes = [
-  { path: '', component: PersonComponent },
-  { path: 'task', component: TaskComponent },
+  //{ path: '', component:  },
+  //{ path: 'task', component:  },
 ];
 
 @NgModule({
--- task-management/src/app/app.module.ts
+++ task-management/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { TaskComponent } from './components/task/task.component';
-import { PersonComponent } from './components/person/person.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent,
-    PersonComponent
   ],
   imports: [
     BrowserModule,
