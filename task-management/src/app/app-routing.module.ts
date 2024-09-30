bdcb98fe9f28bba102e84fac152fa7ef6ff9a541
Jgiraldo <jhonnagira@live.com.mx>
2024-09-30 16:10:25 -0500

    setup routing

--- task-management/src/app/app-routing.module.ts
+++ task-management/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
+import { PersonComponent } from './components/person/person.component';
+import { TaskComponent } from './components/task/task.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: '', component: PersonComponent },
+  { path: 'task', component: TaskComponent },
 ];
 
 @NgModule({
--- task-management/src/app/app.module.ts
+++ task-management/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { TaskComponent } from './components/task/task.component';
+import { PersonComponent } from './components/person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
